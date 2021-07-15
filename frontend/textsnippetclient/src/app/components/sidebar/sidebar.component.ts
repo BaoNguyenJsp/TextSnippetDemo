8df44c123735908b451ae97fa0ea00fe5693e70a
Bao Nguyen <nqbaojsp@gmail.com>
2021-07-15 20:40:00 +0700

    Implement paging and search text snippet in sidebar

--- frontend/textsnippetclient/src/app/components/sidebar/sidebar.component.ts
+++ frontend/textsnippetclient/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GetPaginationTextSnippetAction, GetSelectedTextSnippetAction } from 'app/state/app.action';
+import { AppSelector } from 'app/state/app.selector';
+import { Dispatcher } from 'app/state/dispatcher';
 
 declare const $: any;
 declare interface RouteInfo {
@@ -22,21 +25,59 @@ export class SidebarComponent implements OnInit {
   disableNextButton = false;
   disableBeforeButton = false;
 
-  constructor() { 
+  constructor(private dispatcher: Dispatcher, private appSelector: AppSelector) { 
+    this.appSelector.getPaging$.subscribe(x => {
+      if (!!x.data.length) {
+        this.data = x.data;
+        this.pageSize = x.paging.pageSize;
+        this.pageNumber = x.paging.pageNumber;
+        this.query = x.query;
+        this.disableBeforeButton = x.paging.pageNumber == 0;
+        this.disableNextButton = x.total - ((this.pageNumber + 1) * this.pageSize) <= 0;
+      }
+    });
   }
 
   ngOnInit() {
+    var paging = {
+      query: "",
+      pageSize: this.pageSize,
+      pageNumber: this.pageNumber
+    }
+    this.dispatcher.fire(new GetPaginationTextSnippetAction(paging));
   }
 
   search() {
+    var paging = {
+      query: this.query,
+      pageSize: this.pageSize,
+      pageNumber: this.pageNumber
+    }
+    this.dispatcher.fire(new GetPaginationTextSnippetAction(paging));
   }
 
   getSelected(id: any) {
+    var data = this.data.find(x => x.id == id);
+    this.dispatcher.fire(new GetSelectedTextSnippetAction(data));
   }
 
   moveNext() {
+    this.pageNumber++;
+    var paging = {
+      query: this.query,
+      pageSize: this.pageSize,
+      pageNumber: this.pageNumber
+    }
+    this.dispatcher.fire(new GetPaginationTextSnippetAction(paging));
   }
 
   moveBack() {
+    this.pageNumber--;
+    var paging = {
+      query: this.query,
+      pageSize: this.pageSize,
+      pageNumber: this.pageNumber
+    }
+    this.dispatcher.fire(new GetPaginationTextSnippetAction(paging));
   }
 }
